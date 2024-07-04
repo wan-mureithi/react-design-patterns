d9c95d00e1c2fbb89620d4b586dd2a28e40cb597
wan.mureithi <estherdee27@gmail.com>
2024-07-04 11:11:52 +0300

    styling

--- trainings/src/App.js
+++ trainings/src/App.js
@@ -1,3 +1,4 @@
+import "bulma/css/bulma.css"
 import React from "react"
 import ProfileCard from "./ProfileCard"
 import AlexaImage from "./images/alexa.png"
@@ -7,10 +8,41 @@ import SiriImage from "./images/siri.png"
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
-      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
-      <ProfileCard title="Cortana" handle="@alexa99" image={CortanaImage} />
-      <ProfileCard title="Siri" handle="@alexa99" image={SiriImage} />
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description="Alexa was created by Amazon and helps you buy things"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana9"
+                image={CortanaImage}
+                description="Cortana was created by Microsoft and helps you do things"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={SiriImage}
+                description="Siri was created by Apple and is your personal assistant"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
--- trainings/src/ProfileCard.js
+++ trainings/src/ProfileCard.js
@@ -1,11 +1,22 @@
 import React from "react"
 
-function ProfileCard({ title, handle, image }) {
+function ProfileCard({ title, handle, image, description }) {
   return (
-    <div>
-      <img src={image} alt={title} />
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt={title} />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="title is-6">{handle}</p>
+        </div>
+        <div className="content">
+          <p>{description}</p>
+        </div>
+      </div>
     </div>
   )
 }
