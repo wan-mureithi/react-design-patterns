866f1f237af5a22b0882f6db37bdda42a4126bf1
wan.mureithi <estherdee27@gmail.com>
2024-07-04 10:50:13 +0300

    add images

--- trainings/src/App.js
+++ trainings/src/App.js
@@ -1,13 +1,16 @@
 import React from "react"
 import ProfileCard from "./ProfileCard"
+import AlexaImage from "./images/alexa.png"
+import CortanaImage from "./images/cortana.png"
+import SiriImage from "./images/siri.png"
 
 function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
-      <ProfileCard title="Alexa" handle="@alexa99" />
-      <ProfileCard title="Cortana" handle="@alexa99" />
-      <ProfileCard title="Siri" handle="@alexa99" />
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+      <ProfileCard title="Cortana" handle="@alexa99" image={CortanaImage} />
+      <ProfileCard title="Siri" handle="@alexa99" image={SiriImage} />
     </div>
   )
 }
--- trainings/src/ProfileCard.js
+++ trainings/src/ProfileCard.js
@@ -1,8 +1,9 @@
 import React from "react"
 
-function ProfileCard({ title, handle }) {
+function ProfileCard({ title, handle, image }) {
   return (
     <div>
+      <img src={image} alt={title} />
       <div>Title is {title}</div>
       <div>Handle is {handle}</div>
     </div>
