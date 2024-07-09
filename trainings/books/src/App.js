e3aa44f0df5484ebe6ad168bde39c3e10bd031c9
wan.mureithi <estherdee27@gmail.com>
2024-07-09 13:07:22 +0300

    adding a db and API

--- trainings/books/src/App.js
+++ trainings/books/src/App.js
@@ -1,40 +1,54 @@
-import { useState } from 'react';
-import BookCreate from './components/BookCreate';
-import BookList from './components/BookList';
-
+import { useEffect, useState } from "react"
+import axios from "axios"
+import BookCreate from "./components/BookCreate"
+import BookList from "./components/BookList"
+//"server" : "json-server --port 3001 --watch db.json --host 127.0.0.1"
+const URL = "http://localhost:3001/books"
 function App() {
-  const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState([])
 
-  const editBookById = (id, newTitle) => {
+  const fetchBooks = async () => {
+    const response = await axios.get(URL)
+    setBooks(response.data)
+  }
+  useEffect(() => {
+    fetchBooks()
+  }, [])
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`${URL}/${id}`, { title: newTitle })
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data }
       }
 
-      return book;
-    });
+      return book
+    })
 
-    setBooks(updatedBooks);
-  };
+    setBooks(updatedBooks)
+  }
 
   const deleteBookById = (id) => {
+    axios.delete(`${URL}/${id}`)
     const updatedBooks = books.filter((book) => {
-      return book.id !== id;
-    });
+      return book.id !== id
+    })
 
-    setBooks(updatedBooks);
-  };
+    setBooks(updatedBooks)
+  }
 
-  const createBook = (title) => {
-    const updatedBooks = [
-      ...books,
-      {
-        id: Math.round(Math.random() * 9999),
-        title,
-      },
-    ];
-    setBooks(updatedBooks);
-  };
+  const createBook = async (title) => {
+    const response = await axios.post(URL, { title })
+    console.log(response)
+    // const updatedBooks = [
+    //   ...books,
+    //   {
+    //     id: Math.round(Math.random() * 9999),
+    //     title,
+    //   },
+    // ]
+    // setBooks(updatedBooks)
+  }
 
   return (
     <div className="app">
@@ -42,7 +56,7 @@ function App() {
       <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
